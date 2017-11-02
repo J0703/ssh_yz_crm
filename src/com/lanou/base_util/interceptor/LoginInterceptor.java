fb01bac196d75dfa2e55949025fbd1985f66af3d
yWinter <winter951209@163.com>
2017-11-02 08:36:00 +0800

    完善后的代码 主要完善: 增加用户权限, 用户登录有效时间短, 分包问题 作者: 杨钊 时间: 2017.11.1

---
+++ src/com/lanou/base_util/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.lanou.base_util.interceptor;
+
+import com.lanou.hrd.domain.Staff;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
+import org.apache.struts2.ServletActionContext;
+
+/**
+ * Created by dllo on 17/10/30.
+ */
+public class LoginInterceptor extends MethodFilterInterceptor {
+    @Override
+    protected String doIntercept(ActionInvocation actionInvocation) throws Exception {
+
+        Staff staff = (Staff) ServletActionContext.getContext().getApplication().get("staff");
+        System.out.println(staff);
+        if (staff==null){
+            ServletActionContext.getRequest().setAttribute("msg","您未登录,请先登录!");
+            return Action.LOGIN;
+        }
+        return actionInvocation.invoke();
+    }
+}
---
+++ web/pages/staff/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dllo
+  Date: 17/10/30
+  Time: 下午2:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>跳转</title>
+</head>
+<body>
+    <script>
+       parent.location.href="../frame.jsp";
+    </script>
+</body>
+</html>
